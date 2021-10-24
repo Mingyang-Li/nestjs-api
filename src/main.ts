3e5f82497447023fb89b2a276e850dcf5d0e6402
Mingyang-Li <53138432+Mingyang-Li@users.noreply.github.com>
2021-10-25 12:02:24 +1300

    Generated swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Conduit Blog API')
+    .setDescription('NHTC API')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
+
+  console.log('===============================');
+  console.log(`= ✅ App running on port ${4000} =`);
+  console.log('===============================');
 }
 bootstrap();
